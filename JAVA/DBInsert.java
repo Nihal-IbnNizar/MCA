81658532aaed11aeae074e9d899f4320dfaceabe
MUHAMMED NIHAL <142206176+Nihal-IbnNizar@users.noreply.github.com>
2024-02-21 15:12:33 +0530

    Update and rename DBI.java to DBInsert.java

--- JAVA/DBInsert.java
+++ JAVA/DBInsert.java
@@ -1,7 +1,8 @@
 import java.io.*;
 import java.util.*;
 import java.sql.*;
-class DBI {
+
+class DBInsert {
 
    public static void main(String args[]) {
 	Connection con;
@@ -12,11 +13,8 @@ class DBI {
 	Scanner s = new Scanner(System.in);
 	try {
 	  Class.forName("com.mysql.jdbc.Driver");
-	  //System.out.println("1");
 	  con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ems_db?characterEncoding=utf8","root","");
-	  //System.out.println("2");
 	  st = con.createStatement();
-	  //System.out.println("3");
 	  System.out.println("Enter no name and salary");
 	  no = s.nextInt();
 	  s.nextLine();
@@ -31,7 +29,6 @@ class DBI {
 	  str = str + sal +")";
 	  System.out.println(str);
 	  st.executeUpdate(str);
-	  //System.out.println("4");
 	  System.out.println("One Record is Inserted..");
  	}
 	catch(Exception e) {
